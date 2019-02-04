5cc44ca48004f64f9d5f9bbfeb49c4064481a107
bekeyeva <47300887+bekeyeva@users.noreply.github.com>
2019-02-04 03:20:01 +0100

    s

---
+++ timer.js
@@ -0,0 +1,39 @@
+﻿/*Syntax To Timer */
+var hou = 0;
+var min = 0;
+var second = 0;
+var mili = 0;
+var timerID = null;
+
+function TimerTick() {
+    if (mili >= 9) {
+        mili = 0;
+        if (second >= 59) {
+            second = 0;
+            if (min >= 59) {
+                min = 0;
+                hou += 1;
+            }
+            else {
+                min += 1;
+            }
+        }
+        else {
+            second += 1;
+        }
+    }
+    else {
+        mili += 1;
+    }
+    
+    var MyTimerCount = document.getElementById("myTimer");
+    MyTimerCount.value = hou+':'+min+':'+second + '.' + mili;
+}
+
+function starttimer() {
+    timerID = setInterval(TimerTick, 100, null);
+}
+
+function stoptimer() {
+    if (timerID != null) clearInterval(timerID);
+}
